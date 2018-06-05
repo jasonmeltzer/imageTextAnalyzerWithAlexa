5ddb2efda209998a948f6a75a9c8f65b77009666
Jason Meltzer <jmeltzer@godaddy.com>
2018-06-05 09:45:27 -0700

    More Alexa progress - getting the handler to deal with sessionendedrequests

--- notesSkillIntentRouter.js
+++ notesSkillIntentRouter.js
@@ -57,46 +57,24 @@ const GetNoteIntentHandler = {
 
 const SessionEndedRequestHandler = {
 	canHandle(handlerInput) {
-        return 
-         handlerInput.requestEnvelope.request.type === 'SessionEndedRequest' ||
-         (handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-	      handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+		console.log(handlerInput.requestEnvelope);
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest' ||
+               (handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+	            handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
 	},
 	handle(handlerInput) {
-		const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-	    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-
-		const speakOutput = 'Goodbye';
+		console.log("Goodbye");
 			
-		handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
 		return handlerInput.responseBuilder
 				.withShouldEndSession(true)
-			    .speak(speakOutput)
 			    .getResponse();
 
 	}
 			
 };
 
-/*
-module.exports.route = (event, context, callback) => {
-	
-	 var eventText = JSON.stringify(event, null, 2);
-	 console.log("Received event:", eventText);
-	 
-	 if (event.request.type == "LaunchRequest")
-		 console.log("LaunchRequest");
-	 else if (event.request.type == "IntentRequest") {
-		 if (event.request.intent.name == "MakeNote")
-			 console.log("MakeNote");
-	 }
-     else if (event.request.type == "SessionEndedRequest") 
-    	 console.log("SessionEndedRequest");
-		 
-	 
-};
-*/
+
+
 module.exports.route = skillBuilder
 .addRequestHandlers(
   LaunchRequestHandler,
@@ -106,3 +84,4 @@ module.exports.route = skillBuilder
 //.addRequestInterceptors(LocalizationInterceptor)
 //.addErrorHandlers(ErrorHandler)
 .lambda();
+
