6abfbca1188955dae91e4509ffbc4b0283c7108a
Jason Meltzer <jmeltzer@godaddy.com>
2018-06-05 18:06:37 -0700

    New lambda to parse an image uploaded to S3. Also improved the lambda that responds to Alexa skill so that it can pull some info out of the DynamoDB for one of the uploaded images.

--- notesSkillIntentRouter.js
+++ notesSkillIntentRouter.js
@@ -1,21 +1,20 @@
 'use strict';
 
-var AWS = require('aws-sdk'),
-    documentClient = new AWS.DynamoDB.DocumentClient();
+var AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const Alexa = require('ask-sdk-core');
 const skillBuilder = Alexa.SkillBuilders.custom();
 
-
 const LaunchRequestHandler = {
-    canHandle(handlerInput) {
+    canHandle: (handlerInput) => {
 	    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
 	},
-	handle(handlerInput) {
+	handle: (handlerInput) => {
 	    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
 		const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
 
-		const speakOutput = 'Welcome. To get a note, say \"Get one.\"';
+		const speakOutput = 'Welcome. To get a note, say \"Get a note.\"';
 		
 		handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
 
@@ -29,26 +28,62 @@ const LaunchRequestHandler = {
 };
 
 const GetNoteIntentHandler = {
-	canHandle(handlerInput) {
+	canHandle: (handlerInput) => {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
                handlerInput.requestEnvelope.request.intent.name === 'GetNote';
 	},
-	handle(handlerInput) {
+	handle: (handlerInput) => {
 		const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
 	    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
 
 		console.log(handlerInput.requestEnvelope);
-			
-		const speakOutput = 'I\'m still working on that. More soon.';
-			
-		handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+		
+		var params = {
+	        TableName: process.env.DYNAMODB_TABLE,
+	        IndexName: 'userIndex', // You have to add an index for this column if you want to query on it.
+	        KeyConditionExpression: "#user = :u",
+	        ExpressionAttributeNames:{
+	            "#user": "user"
+	        },
+	        ExpressionAttributeValues: {
+	            ":u": "jason" // TODO: Make this something real.
+	        }
+	    };	
+		
+		var speakOutput;
+		return new Promise((resolve, reject) => {
 
-		return handlerInput.responseBuilder
-			    .speak(speakOutput)
-				.withShouldEndSession(false)
-			    .withSimpleCard('title', 'cardText')
-			    .getResponse();
+		    dynamoDb.query(params, (error, result) => {
+		    	if (error || result.Items.length == 0) {
+				    console.error(error);
+				    speakOutput = "Couldn\'t find a note.";
+				} else {
+					console.log(result.Items.length);
+					
+					// Randomly choose one of the notes, for now.
+					var item = result.Items[Math.floor(Math.random() * result.Items.length)];
+					
+					speakOutput = item.noteText.join(' ');
+					
+				}
 
+				console.log(speakOutput);
+
+				handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+				resolve(handlerInput.responseBuilder
+						    .speak(speakOutput)
+							.withShouldEndSession(false)
+						    .withSimpleCard('A note', 'cardText')
+						    .getResponse()
+				);
+		    });
+
+			
+		});
+
+
+	     
 	}
 			
 };
@@ -56,13 +91,13 @@ const GetNoteIntentHandler = {
 
 
 const SessionEndedRequestHandler = {
-	canHandle(handlerInput) {
+	canHandle: (handlerInput) => {
 		console.log(handlerInput.requestEnvelope);
         return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest' ||
                (handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
 	            handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
 	},
-	handle(handlerInput) {
+	handle: (handlerInput) => {
 		console.log("Goodbye");
 			
 		return handlerInput.responseBuilder
---
+++ parseNoteImage.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const AWS = require('aws-sdk'); 
+var rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
+
+module.exports.parse = (event, context, callback) => {
+
+	console.log(event);
+	
+	// Derive the bucket and filename from the event
+    var bucket = event.Records[0].s3.bucket.name;
+    var key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    var params = {
+        Bucket: bucket,
+        Key: key,
+    }
+    console.log(bucket);
+    console.log(key);
+ 
+    var params = {
+        Image: {
+    		S3Object: {
+    		    Bucket: bucket,
+    		    Name: key
+    		}
+        }
+    };
+    
+    console.log(params);
+    rekognition.detectText(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {								  // successful response
+        	console.log(data);   
+
+        	var textLines = [];
+        	
+        	for (var i = 0, len = data.TextDetections.length; i < len; i++) {
+        	    var textDetection = data.TextDetections[i];
+        	    console.log(textDetection);
+        	
+        	    // Only interested in LINE types right now, which should include all of the WORD types
+        	    if (textDetection.Type === 'LINE') {
+        	    	textLines.push(textDetection.DetectedText);
+        	    }
+        	}
+        	
+        	var dynamoParams = {
+        	    TableName: process.env.DYNAMODB_TABLE,
+        	    Item: {
+        	      id: uuid.v1(),
+        	      noteText: textLines,
+        	      user: 'jason' // TODO: Do something real here.
+        	    }
+        	};   
+        	// write the note to the database
+        	dynamoDb.put(dynamoParams, (error) => {
+        	    // handle potential errors
+        	    if (error) {
+        	        console.error(error);
+        	    }
+        	});
+        }
+    });    
+    
+    
+};
